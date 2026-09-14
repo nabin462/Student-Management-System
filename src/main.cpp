4ec036ae0c642fa7f4bb5852eb2de0a7afcc1873
nabin <budhathokinabin700@gmail.com>
2026-09-14 15:25:00 +0545

    feat: complete student CRUD operations

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ int main(){
         cout<<"2: display all student"<<endl;
         cout<<"3: search student"<<endl;
         cout<<"4: Delete student"<<endl;
-        cout<<"5: Exit"<<endl;
+        cout<<"5: update student "<<endl;
+        cout<<"6: Exit"<<endl;
         cout<<"Enter your choice"<<endl;
         cin>>choice;
         switch(choice){
@@ -49,6 +50,8 @@ int main(){
     }
      break;
 
+     
+
      case 4:{
      int uid;
      cout<<"enter the student id"<<endl;
@@ -70,8 +73,31 @@ int main(){
      }
     }
      break;
-     
+
      case 5:
+     {
+     int sid;
+    
+     cout<<"enter the student id"<<endl;
+     cin>>sid;
+      bool found=false;
+     for(size_t i=0;i<students.size();i++){
+     if(sid==students[i].getid()){
+      cout<<"student is found"<<endl;
+      students[i].get();
+      found=true;
+      break;
+
+     }
+
+    }
+    if(!found){
+      cout<<"student is not fond"<<endl;
+    }
+  }
+     break;
+     
+     case 6:
      exit(0);
      
 
